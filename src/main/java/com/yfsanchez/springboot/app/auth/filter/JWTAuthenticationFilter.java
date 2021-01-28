0d39b1a9f5c4aa9d73da650c3422708529061e75
yfigueroa <yfsanchez@gmail.com>
2021-01-28 07:45:58 -0300

    264 Manejando errores de authentication
    264 Manejando errores de authentication

--- src/main/java/com/yfsanchez/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/yfsanchez/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -118,6 +118,23 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         response.setContentType("application/json");
 		//super.successfulAuthentication(request, response, chain, authResult);
 	}
+
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("mensaje", "Error en password o username.");
+		body.put("error", failed.getMessage());
+		
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(401);
+        response.setContentType("application/json");
+//		super.unsuccessfulAuthentication(request, response, failed);
+	}
+	
+	
 	
 	
 
